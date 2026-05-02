3110d2cf7509ab186fe8f81ad323520eeda5b087
yesica rengifo <yesicarengifo2003@gmail.com>
2026-05-02 01:41:17 -0500

    + cambio de seguimiento comunitario

---
+++ seguimiento-comunitario.js
@@ -0,0 +1,15 @@
+let semana = 1
+const semanasdeSeguimiento = 4
+let propuestasatendidas = 0
+
+console.log("seguimiento comunitario semana11")
+
+while (semana <= semanasdeSeguimiento){
+    console .log("semananumero.",semana)
+propuestasatendidas = propuestasatendidas + 2
+console . log("propuestasatendidas hasta el momento:",propuestasatendidas)
+semana = semana + 1
+}
+console .log(" seguimiento comunitario finalizado.")
+console .log ("total de propuestasatendisas", propuestasatendidas)
+
